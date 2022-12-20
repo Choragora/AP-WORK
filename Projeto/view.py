86efc0bb319f8d0dc150441d4b0390c1f73d7b9d
afonsoraimundo16 <ramajal.afonso@gmail.com>
2022-12-20 20:45:00 +0000

    Update view.py

--- Projeto/view.py
+++ Projeto/view.py
@@ -3,3 +3,4 @@ from controller import *
 def main():
     print("Eu sei que nao posso fazer isso. ola")
     print("Es bue gay")
+     print("Es bue gay") print("Es bue gay") print("Es bue gay") print("Es bue gay") print("Es bue gay") print("Es bue gay")
