dbcf293e46c132e0d29f0af64e22ad3a848e1d84
Tomasalex30 <120526991+Tomasalex30@users.noreply.github.com>
2022-12-21 20:06:24 +0000

    Mudanças

---
+++ Projeto/model.py
@@ -0,0 +1,9 @@
+dicionario_jogadores = {
+    "Nome": "Pedro", "Idade": 14, "Sexo": "Masculino",
+    "Nome": "Andre", "Idade": 14, "Sexo": "Masculino",
+    "Nome": "Tiago", "Idade": 14, "Sexo": "Masculino",
+    "Nome": "ROdrigo", "Idade": 14, "Sexo": "Masculino",
+    }
+
+for elemento in dicionario_jogadores.keys():
+    print(elemento)
--- Projeto/view.py
+++ Projeto/view.py
@@ -24,33 +24,37 @@ while True:
     
     opcao_menu = input("Introduza a sua opção: ").lower()
    
-    if opcao_menu == "ij":
+    if opcao_menu == "ij": #iniciar jogo
         print("1")
 
-    elif opcao_menu == "rj":
+    elif opcao_menu == "rj": #registar jogador
         nome_registar = input("Introduza o nome a registar: ").lower()
-        while nome_registar in lista_jogadores:
+        while nome_registar in lista_jogadores: #loop que só é quebrado quando o jogador nao está na lista.
             print("Jogador existente.")
             nome_registar= input("Introduza o nome a registar: ").lower()
         registar_jogador(nome_registar, lista_jogadores)
-        print("Jogador registado com  sucesso.")
+        print("Jogador registado com sucesso.")
 
         #ver ficheiros e armazenar os nomes dentro dos ficheiros
             
-    elif opcao_menu == "ej":
-        if len(lista_jogadores) == 0:
+    elif opcao_menu == "ej": #eliminar jogador
+        if len(lista_jogadores) == 0: #verificação de número de jogadores, se estiver vazia é resultante uma saida com insucesso
             print("Não existem jogadores registados")
         else:
             nome_remover = input("Introduza o nome a remover: ").lower()
-            while nome_remover not in lista_jogadores:
+            while nome_remover not in lista_jogadores: #loop que só é quebrado quando o jogador está na lista.
                 print("Jogador não existente.")
                 nome_remover = input("Introduza o nome a eliminar: ").lower()
             remover_jogador(nome_remover, lista_jogadores)
             print("Jogador removido com sucesso.")
-            
 
+            # Saída com insucesso: Quando o jogador indicado não se encontra registado.
+               
     elif opcao_menu == "lj":
-        pass
+        if len(lista_jogadores) == 0:
+            print("Não existem jogadores registados.")
+        else:
+            
 
     elif opcao_menu == "sm":
         break
