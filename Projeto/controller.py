c02d4d76de55d9ab49ed9ccec5babc5f9cd249a4
Tomasalex30 <120526991+Tomasalex30@users.noreply.github.com>
2023-01-06 11:15:46 +0000

    comemntarios
    adicionar comentarios feito exclusivamente pelo tomas e o alexandre

--- Projeto/controller.py
+++ Projeto/controller.py
@@ -1,36 +1,34 @@
-def registar_jogador(nome_registar, lista_jogadores): # função de adicionar jogadores dento da lista de jogadores
+def registar_jogador(nome_registar, lista_jogadores):                                       #função de adicionar jogadores dento da lista de jogadores
   lista_jogadores.append(nome_registar)
   return lista_jogadores
 
 
-def remover_jogador(nome_remover, lista_jogadores): # função de remover jogadores da lista de jogadores
+def remover_jogador(nome_remover, lista_jogadores):                                         #função de remover jogadores da lista de jogadores
   lista_jogadores.remove(nome_remover)
   return lista_jogadores
 
 
-def listar_jogadores(lista_jogadores, lista_partidas, lista_vitorias, lista_lj): # função que faz a iteração de cada uma das três listas, passando em cada posição igual entre ambas as listas,
-  for i in range(len(lista_jogadores)):                                          # depois o valor de i de ambas as listas na posição n (primeiro loop n=0, loops seguintes n+1) são armazenadas em  
-    jogadores = lista_jogadores[i]                                               # uma lista, e essa lista é armazenada em outra lista teno uma lista base para outras listas.
-    partidas = lista_partidas[i]                                                 # este processo repete-se ate a ultima posição de cada lista, atençao qeu cada lista tem que apresentar o mesmo numero de indices
+def listar_jogadores(lista_jogadores, lista_partidas, lista_vitorias, lista_lj):            #função que faz a iteração de cada uma das três listas, passando em cada posição igual entre ambas as listas,
+  for i in range(len(lista_jogadores)):                                                     #depois o valor de i de ambas as listas na posição n (primeiro loop n=0, loops seguintes n+1) são armazenadas em  
+    jogadores = lista_jogadores[i]                                                          #uma lista, e essa lista é armazenada em outra lista teno uma lista base para outras listas.
+    partidas = lista_partidas[i]                                                            #este processo repete-se ate a ultima posição de cada lista, atençao qeu cada lista tem que apresentar o mesmo numero de indices
     vitorias = lista_vitorias[i]
     lista_lj.append([jogadores, partidas, vitorias])
   return lista_lj
 
 
-def nome_jogadores_ij (jogador_1, jogador_2, jogadores_jogo):
-
-
+def nome_jogadores_ij (jogador_1, jogador_2, jogadores_jogo):                               #funcao que adiciona os nomes dos 2 jogadores dentro de uma lista e ordena os mesmos alfabeticamente
   jogadores_jogo.append(jogador_1)
   jogadores_jogo.append(jogador_2)
   jogadores_jogo.sort()
   return jogadores_jogo
 
 
-def criar_tabela(comprimento,altura, tabela):
+def criar_tabela(comprimento,altura, tabela):                                               #funcao que queria os slots da tabela
   for i in range (altura):
     tabela.append(["|___|"] *comprimento)
 
-def desenhar_tabela(comprimento, tabela, valor_lateral):
+def desenhar_tabela(comprimento, tabela, valor_lateral):                                    #funcao que desenha a tabela vazia 
   for i in range(comprimento):
       print(f"    {i}", end="")
   print()  
--- Projeto/view.py
+++ Projeto/view.py
@@ -38,28 +38,28 @@ def main():
             ########### INICIAR JOGO ############
 
 
-        if opcao[0] == "ij":                                                                                #iniciar jogo
-            comprimento = int(opcao[3])
+        if opcao[0] == "ij":                                                                                            #iniciar jogo
+            comprimento = int(opcao[3])                                 
             altura = int(opcao[4])
             sequencia_vencedora = int(opcao[5])
                                                                                        
-            if len(lista_jogadores) > 1:                                                                  
-                jogadores_jogo = []
+            if len(lista_jogadores) > 1:                                                                                #se a lista tiver pelo menos dois jogadores registados                                  
+                jogadores_jogo = []                                                     
                 ## Nomes ##
-                if (opcao[1] not in lista_jogadores) or (opcao[2] not in lista_jogadores):    
+                if (opcao[1] not in lista_jogadores) or (opcao[2] not in lista_jogadores):                              #verifica se os jogadores estao registados
                     print("Jogador não registado.")
                 else:    
-                    nome_jogadores_ij(opcao[1], opcao[2], jogadores_jogo)
+                    nome_jogadores_ij(opcao[1], opcao[2], jogadores_jogo)                                   
                 
                 ## Dimensoes ##
-                if (comprimento < altura) or (comprimento/2 > altura): 
+                if (comprimento < altura) or (comprimento/2 > altura):                                                  #verifica as condições da dimensao da grelha
                     print("Dimensões de grelha invalidas.")
                 else:  
-                    criar_tabela(comprimento, altura, tabela)
+                    criar_tabela(comprimento, altura, tabela)                                                           #cria os slots com os inseridos
                     print(tabela)
 
                 ## Sequencia vencedora ##
-                if sequencia_vencedora == 1 or sequencia_vencedora == 2 or sequencia_vencedora > comprimento:
+                if sequencia_vencedora == 1 or sequencia_vencedora == 2 or sequencia_vencedora > comprimento:           #verifica as condições da sequencia vencedora
                     print("Tamanho de sequência invalido.")
                 else:
                     pass
@@ -74,11 +74,11 @@ def main():
             ########### REGISTAR JOGADOR ############
 
 
-        elif opcao[0] == "rj":                                                                            #registar jogador
-            if opcao[1] in lista_jogadores:                                                         #loop que só é quebrado quando o jogador nao está na lista.
+        elif opcao[0] == "rj":                                                                                          #registar jogador
+            if opcao[1] in lista_jogadores:                                                                             #loop que só é quebrado quando o jogador nao está na lista.
                 print("Jogador existente.")
             else:
-                registar_jogador(opcao[1], lista_jogadores)                                                #registar jogadores, adicionando a lista de jogadores
+                registar_jogador(opcao[1], lista_jogadores)                                                             #registar jogadores, adicionando a lista de jogadores
                 print("Jogador registado com sucesso.")
                 print(lista_jogadores)
             
@@ -86,14 +86,14 @@ def main():
             ########### ELIMINAR JOGADOR ############
            
             
-        elif opcao[0] == "ej":                                                                            #eliminar jogador
-            if len(lista_jogadores) == 0:                                                                   #verificação de número de jogadores, se estiver vazia é resultante uma saida com insucesso
+        elif opcao[0] == "ej":                                                                                          #eliminar jogador
+            if len(lista_jogadores) == 0:                                                                               #verificação de número de jogadores, se estiver vazia é resultante uma saida com insucesso
                 print("Não existem jogadores registados")
             else:
-                if opcao[1] not in lista_jogadores:                                                  #loop que só é quebrado quando o jogador está na lista.
+                if opcao[1] not in lista_jogadores:                                                                     #loop que só é quebrado quando o jogador está na lista.
                     print("Jogador não existente.")
                 else:
-                    remover_jogador(opcao[1], lista_jogadores)                                              #remover jogadores, removendo da lista de jogadores
+                    remover_jogador(opcao[1], lista_jogadores)                                                          #remover jogadores, removendo da lista de jogadores
                     print("Jogador removido com sucesso.")
                     print(lista_jogadores)
 
@@ -104,49 +104,49 @@ def main():
             ########### LISTAR JOGADOR ############
 
                 
-        elif opcao[0] == "lj":                                                                            #listar jogadores              
-            if len(lista_jogadores) == 0:                                                                   #verificação de número de jogadores, se estiver vazia é resultante uma saida com insucesso
+        elif opcao[0] == "lj":                                                                                          #listar jogadores              
+            if len(lista_jogadores) == 0:                                                                               #verificação de número de jogadores, se estiver vazia é resultante uma saida com insucesso
                 print("Não existem jogadores registados.")
             else:
-                lista_lj = []                                                                               #serve para reiniciar os valores caso seja adicionado um nojogo jogador
+                lista_lj = []                                                                                           #serve para reiniciar os valores caso seja adicionado um nojogo jogador
                 listar_jogadores(lista_jogadores, lista_partidas, lista_vitorias, lista_lj) 
-                for jogadores, partidas, vitorias in lista_lj:                                              #ve a primeira lista dentro da lista_lj
+                for jogadores, partidas, vitorias in lista_lj:                                                          #ve a primeira lista dentro da lista_lj
                     print(f"Nome: {jogadores} / Partidas: {partidas} / Vitórias: {vitorias}")
 
             ########### DETALHES JOGO ############
 
-        elif opcao[0] == "dj":
+        elif opcao[0] == "dj":                                                                                          #detalhes do jogo
             pass
 
             ########### DESISTIR #############
 
-        elif opcao[0] == "d":
-            pass
+        elif opcao[0] == "d":                                                                                           #desistir
+            pass    
             
             ########### COLOCAR PEÇA #############
 
-        elif opcao[0] == "cp":
+        elif opcao[0] == "cp":                                                                                          #colocar peça
             pass
 
             ########### VISUALIZAR RESULTADO #############
 
-        elif opcao[0] == "v":
+        elif opcao[0] == "v":                                                                                           #ver tabela
             valor_lateral = 0
             desenhar_tabela(comprimento, tabela, valor_lateral)
 
             ########### GRAVAR #############
         
-        elif opcao[0] == "g":
+        elif opcao[0] == "g":                                                                                           #gravar
             pass
         
             ########## LER #############
 
-        elif opcao[0] == "l":
+        elif opcao[0] == "l":                                                                                           #ler
             pass
             
             ########### SAIR DO MENU ############
 
-        elif opcao[0] == "sm":
+        elif opcao[0] == "sm":                                                                                          #sair menu 
             break
         
         else:
